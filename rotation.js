bfe84ee80b1063db9d311f6e258a50c126253571
VincentK <32392302+vincentk191@users.noreply.github.com>
2017-10-08 14:03:12 +0200

    Miguel (#3)
    * added navigation bar
    
    * map edit
    
    * fix map and nav bar
    
    * updated map and animation

---
+++ rotation.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CONTACT US</title>
+</head>
+<link rel="stylesheet" type="text/css" href="buber.css">
+<body>
+    <nav class="navigation">
+        <ul>
+            <li><a href="contactUs.html">CONTACT US</a></li>
+            <li><a href="about.html">ABOUT</a></li>
+            <li><a href="services.html">SERVICES</a></li>
+            <li><a href="home.html">HOME</a></li>
+        </ul>
+        <hr>
+    </nav>
+    <h2>Address 1:</h2>
+    <h3>Johan Huizingalaan 763A, <br>
+    1066 VH Amsterdam</h3>
+    <h2>Address 2</h2>
+    <h3>John M. Keynesplein 12-46, <br>
+    1066 EP Amsterdam</h3>
+    <div id="map"></div>
+
+    <script>
+        function initMap (){
+            var location = {lat: -25.363, lng: 131.044};
+            var math = new google.maps.Map(document.getElementById("map"),{
+                zoom: 4,
+                center: location
+            });
+        }
+    </script>
+</body>
+<footer>
+	
+</footer>
+</html>
