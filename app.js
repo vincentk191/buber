4e9367c0a70b3ceb8bd632b583bdfc87d438d94b
atlassi <atlassi>
2017-10-20 12:33:03 +0200

    Edited the html and app.js. Removed unnecessary stuff

--- app.js
+++ app.js
@@ -2,21 +2,12 @@ const express = require('express');
 const app = express();
 const path = require("path");
 
-
 app.use(express.static('public'));
-app.use(express.static('buber.css'));
-
-
 app.get('/', function (req, res){
-	res.sendFile(path.resolve('./public/services.html'));
+	res.sendFile(path.resolve('public'));
 
 })
-
-
-
-
-
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000! <br> hoi hoi')
+  console.log('Thomas said: Not necessary.')
 })
 
