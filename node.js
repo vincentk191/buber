092e5535328a5128db4e77eff60d0d0ffa40e15b
VincentK <vincent.kesumo@gmail.com>
2017-12-06 21:41:50 +0100

    sass_d

--- node.js
+++ node.js
@@ -6,29 +6,24 @@ const path = require('path');
 app.use(express.static(path.join(__dirname + '/public/css')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',function(request,response){
-	response.sendFile(path.resolve('./public/home.html'));
-
+app.get('/',function(req,res){
+	res.sendFile(path.resolve('./public/home.html'));
 });
 
 app.get('/home',function(request,response){
 	response.sendFile(path.resolve('./public/home.html'));
-
 });
 
 app.get('/contactUs',function(request,response){
 	response.sendFile(path.resolve('./public/contactUs.html'));
-
 });
 
 app.get('/about',function(request,response){
 	response.sendFile(path.resolve('./public/about.html'));
-
 });
 
 app.get('/services',function(request,response){
 	response.sendFile(path.resolve('./public/services.html'));
-
 });
 
 var server = app.listen(3000);
